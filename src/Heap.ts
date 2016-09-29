4fe7e9ad92cfe4c96a7901ca9fe497a9001a2f68
Brian Folts <brian.folts@gmail.com>
2016-09-28 19:53:01 -0600

    feat(lint): add linting support

--- src/Heap.ts
+++ src/Heap.ts
@@ -1,56 +1,56 @@
 
 export class Heap<T> {
 
-	private storage: T[] = [undefined];
+    private storage: T[] = [undefined];
 
-	constructor(private comparator: (item1: T, item2: T) => boolean = (item1: T, item2: T) => item1 < item2) {
+    constructor(private comparator: (item1: T, item2: T) => boolean = (item1: T, item2: T) => item1 < item2) {
 
-	}
+    }
 
-	public empty(): boolean {
-		return this.storage.length === 1;
-	}
+    public empty(): boolean {
+        return this.storage.length === 1;
+    }
 
-	public add(item: T): void {
-		this.storage.push(item);
-		this.swim(this.storage.length - 1);
-	}
+    public add(item: T): void {
+        this.storage.push(item);
+        this.swim(this.storage.length - 1);
+    }
 
-	public remove(): T {
-		if (this.empty()) {
-			throw new Error("Empty Heap");
-		}
-		this.swap(0, 1);
-		var res = this.storage.shift();
-		this.sink(1);
-		return res;
-	}
+    public remove(): T {
+        if (this.empty()) {
+            throw new Error("Empty Heap");
+        }
+        this.swap(0, 1);
+        const res = this.storage.shift();
+        this.sink(1);
+        return res;
+    }
 
-	private sink(index: number): void {
-		var check = index * 2;
-		var check2 = check + 1;
-		if (check < this.storage.length && this.comparator(this.storage[index], this.storage[check])) {
-			this.swap(check, index);
-			this.sink(check);
-		} else if (check2 < this.storage.length && this.comparator(this.storage[index], this.storage[check2])) {
-			this.swap(check2, index);
-			this.sink(check2);
-		}
-	}
+    private sink(index: number): void {
+        const check = index * 2;
+        const check2 = check + 1;
+        if (check < this.storage.length && this.comparator(this.storage[index], this.storage[check])) {
+            this.swap(check, index);
+            this.sink(check);
+        } else if (check2 < this.storage.length && this.comparator(this.storage[index], this.storage[check2])) {
+            this.swap(check2, index);
+            this.sink(check2);
+        }
+    }
 
-	private swap(index1: number, index2: number): void {
-		var temp: T = this.storage[index1];
-		this.storage[index1] = this.storage[index2];
-		this.storage[index2] = temp;
-	}
+    private swap(index1: number, index2: number): void {
+        const temp: T = this.storage[index1];
+        this.storage[index1] = this.storage[index2];
+        this.storage[index2] = temp;
+    }
 
-	private swim(index: number): void {
-		var half = index / 2 | 0;
-		if (half > 0 && this.comparator(this.storage[half], this.storage[index])) {
-			this.swap(half, index);
-			this.swim(half);
-		}
-	}
+    private swim(index: number): void {
+        const half = index / 2 | 0;
+        if (half > 0 && this.comparator(this.storage[half], this.storage[index])) {
+            this.swap(half, index);
+            this.swim(half);
+        }
+    }
 
 }
 
--- test/Heap.ts
+++ test/Heap.ts
@@ -4,12 +4,12 @@ import {Heap} from "../src/Heap";
 
 describe("Heap", () => {
 
-	it("should know if empty", () => {
-		let heap: Heap<number> = new Heap<number>();
-		assert.isOk(heap.empty(), "heap is empty upon creation");
-	        heap.add(1);
-                assert.isNotOk(heap.empty(), "heap is not empty, has one item");
-	});
+    it("should know if empty", () => {
+        let heap: Heap<number> = new Heap<number>();
+        assert.isOk(heap.empty(), "heap is empty upon creation");
+        heap.add(1);
+        assert.isNotOk(heap.empty(), "heap is not empty, has one item");
+    });
 
 });
 
